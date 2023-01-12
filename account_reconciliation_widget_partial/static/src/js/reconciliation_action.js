dc8981b57f738cfa446181badd511c5bcd88ba3a
Justinas Orechovas <jorechovas@archeti.com>
2023-01-12 14:38:36 -0500

    [IMP] account_reconciliation_widget_partial: black, isort, prettier

--- account_reconciliation_widget_partial/static/src/js/reconciliation_action.js
+++ account_reconciliation_widget_partial/static/src/js/reconciliation_action.js
@@ -1,13 +1,12 @@
 odoo.define("account_reconcile_partial.ReconciliationClientAction", function (require) {
-
-    var action = require('account.ReconciliationClientAction');
+    var action = require("account.ReconciliationClientAction");
 
     action.StatementAction.include({
         custom_events: _.extend({}, action.StatementAction.prototype.custom_events, {
-            get_partial_amount: '_onActionPartialAmount',
-            partial_reconcile: '_onAction',
+            get_partial_amount: "_onActionPartialAmount",
+            partial_reconcile: "_onAction",
         }),
-        _onActionPartialAmount: function(event) {
+        _onActionPartialAmount: function (event) {
             var self = this;
             var handle = event.target.handle;
             var line = this.model.getLine(handle);
